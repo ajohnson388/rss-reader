69d68919ccae90e3ca1a5511e4a3e4e0d54e69ef
ajohnson <AJohnson@haystax.com>
2016-09-04 11:29:22 -0400

    Initialized the Xcode project.

---
+++ RSS Reader/RSS Reader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RSS Reader
+//
+//  Created by Andrew Johnson on 9/4/16.
+//  Copyright © 2016 Andrew Johnson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
